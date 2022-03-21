34994c673503f6d1e4e102cccba9b1b037f459d1
Andej Nesterenko <mentatus.n@gmail.com>
2022-03-21 22:50:47 +0300

    урок 1, ветвление if/try/when(раньше было switch)

--- app/src/main/java/com/gb/k_1919_2/lesson1/Data.kt
+++ app/src/main/java/com/gb/k_1919_2/lesson1/Data.kt
@@ -3,22 +3,44 @@ package com.gb.k_1919_2.lesson1
 import android.view.View
 
 
-data class Data(val id:Int,var age:Int,val weight:Int=20)
+data class Data(val id: Int, var age: Int, val weight: Int = 20)
 
 
+enum class TestEnum {
+    test1,
+    test2,
+    test3,
+    test4,
+}
 
-
-object Database{
-    fun getTest():String{
-        if(0==0){
-            return "test"
-        }else{
-            return "мир сошел с ума"
+object Database {
+    fun getTestIf(): String {
+        val result = if (0 == 0) {
+            val f12 = 1 + 235423
+            val f2 = 1 + 235423
+            val f3 = 1 + 235423
+            "test"
+        } else {
+            "мир сошел с ума"
         }
 
-        val impl = object : View.OnClickListener{
-            override fun onClick(v: View?) {
-            }
+        try {
+
+        }catch (e:Throwable){
+
+        }finally {
+            //закрыть соединение с сервером
         }
+        return result
+    }
+
+    fun getTestWhen(input: TestEnum): String {
+        val result = when (input) { //switch
+            TestEnum.test1 -> "1"
+            TestEnum.test2 -> "2"
+            TestEnum.test3 -> "3"
+            TestEnum.test4 -> "4"
+        }
+        return result
     }
 }
