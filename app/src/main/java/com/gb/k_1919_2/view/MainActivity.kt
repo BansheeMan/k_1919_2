b60ed49a850dab1eefc7998c1259e8549f6d6713
Andej Nesterenko <mentatus.n@gmail.com>
2022-04-21 20:19:24 +0300

    урок 8 начали, освежили SharedPreferences

--- app/src/main/java/com/gb/k_1919_2/view/MainActivity.kt
+++ app/src/main/java/com/gb/k_1919_2/view/MainActivity.kt
@@ -1,31 +1,22 @@
 package com.gb.k_1919_2.view
 
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
-import android.os.Looper
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.gb.k_1919_2.R
-import com.gb.k_1919_2.lesson3.Lesson3
-import com.gb.k_1919_2.lesson3.someViewGroup
-import com.gb.k_1919_2.lesson4.BaseImpl
-import com.gb.k_1919_2.lesson4.BossDelegate
 import com.gb.k_1919_2.lesson4.Lesson4
-import com.gb.k_1919_2.lesson4.Speakable
 import com.gb.k_1919_2.lesson6.MainService
 import com.gb.k_1919_2.lesson6.MyBroadcastReceiver
 import com.gb.k_1919_2.lesson6.ThreadsFragment
+import com.gb.k_1919_2.repository.dto.WeatherDTO
 import com.gb.k_1919_2.utlis.KEY_BUNDLE_ACTIVITY_MESSAGE
-import com.gb.k_1919_2.utlis.KEY_WAVE
+import com.gb.k_1919_2.utlis.KEY_SP_FILE_NAME_1_KEY_IS_RUSSIAN
+import com.gb.k_1919_2.utlis.KEY_SP_FILE_NAME_1
 import com.gb.k_1919_2.view.weatherlist.WeatherListFragment
 
 class MainActivity : AppCompatActivity() {
@@ -45,6 +36,16 @@ class MainActivity : AppCompatActivity() {
         //registerReceiver(receiver, IntentFilter(KEY_WAVE))
         registerReceiver(receiver, IntentFilter("android.intent.action.AIRPLANE_MODE"))
         //LocalBroadcastManager.getInstance(this).registerReceiver(receiver, IntentFilter("myaction"))
+
+        val sp = getSharedPreferences(KEY_SP_FILE_NAME_1, Context.MODE_PRIVATE)
+
+        val editor =  sp.edit()
+        editor.putBoolean(KEY_SP_FILE_NAME_1_KEY_IS_RUSSIAN,true)
+        editor.apply()
+
+
+        val defaultValueIsRussian = true
+        sp.getBoolean(KEY_SP_FILE_NAME_1_KEY_IS_RUSSIAN,defaultValueIsRussian)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
